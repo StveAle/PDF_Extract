79c3eee65164649fc2a08c223154cfacc602dba9
Steve Melgar <steve16jr@gmail.com>
2022-05-01 23:30:10 -0600

    agregada la función wordPDFdic

--- functions.py
+++ functions.py
@@ -37,4 +37,53 @@ def saveListTxt(lista):
     with open(f"./file_{ahora}.txt", "w") as output:
         for row in lista:
             output.write(str(row)+'\n')
-            
+
+#Devuelve un string a partir de una frase y dos strings de referencia que
+#delimitan el inicio y el fin del string. No toma los string de incio y fin.
+#Nota: el código es mejorable.
+def wordBetween(pStart,pEnd,phrase):
+    char1=pStart[0]
+    char2=pEnd[0]
+    val1,val2=False,False
+    finalWord=''
+    
+    for i, char in enumerate(phrase):
+        if char1==char and val1==False:
+            for j, letter in enumerate(pStart):
+                if letter==phrase[i+j]: val1=True
+                else: 
+                    val1=False
+                    break
+
+        if val1: finalWord+=char
+
+        if char2==char and val2==False and val1==True:
+            for j, letter in enumerate(pEnd):
+                if letter==phrase[i+j]: val2=True
+                else: 
+                    val2=False
+                    break
+        
+        if val2: break
+
+    finalWord=finalWord[len(pStart):]
+    return finalWord
+
+#A partir de una lista de coordenadas XY y la ubicación de un archivo PDF busca
+#todas las palabras que estén en esas coordenadas hoja por hoja y almacena todas
+#esas palabras en un diccionario con la metadata de cada palabra y retorna el
+#diccionario. 
+def wordPDFdic (listCC,pdfAddress):
+    file=fitz.open(pdfAddress)
+    wordDic={}
+
+    for i, page in enumerate(file.pages(), start=1):
+        wordList=[]
+        xy=[1,2]
+        for word in page.get_text('words'):
+            xy[0]=word[0]
+            xy[1]=word[1]
+            if xy in listCC: wordList.append(word[4])
+        wordDic[i]=wordList
+
+    return wordDic

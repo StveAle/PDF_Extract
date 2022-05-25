989d5a76b0472e8f601108c40002c899c8e3054f
Steve Melgar <steve16jr@gmail.com>
2022-05-24 21:26:12 -0600

    ordenando los frames

--- interface.py
+++ interface.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from turtle import left, right
+from tkinter import ttk
 from functions import *
 
 class v1Window(tk.Frame):
@@ -10,7 +10,6 @@ class v1Window(tk.Frame):
     def __init__(self,master=None):
         super().__init__(master, width=650, height=450, bg='#C0CBFC')
         self.master=master
-        #self.pack()
         self.createWidgets()
 
     #Creamos los elementos que tendrá el frame
@@ -18,22 +17,30 @@ class v1Window(tk.Frame):
         self.frameMain=tk.Frame(self.master)
         self.frameMain.grid(row=0,column=3,padx=(10,10),pady=(5,5) )
 
-        tk.Label(self.frameMain,text='Dirección: ',padx=5,pady=5).grid(row=0,column=0)
-        tk.Label(self.frameMain,text='Cantidad de columnas: ',padx=5,pady=5).grid(row=1,column=0)      
+        self.frameTop=tk.Frame(self.frameMain)
+        self.frameTop.grid(row=0,columnspan=3)
 
+        self.directlbl=tk.Label(self.frameTop,text='Dirección: ',padx=5,pady=5)
         val='C: '
-        self.txt1=tk.Entry(self.frameMain,width=40,textvariable=val)
-        self.txt1.grid(row=0,column=2)
+        self.addresstxt=tk.Entry(self.frameTop,width=50,textvariable=val)
+        self.buscarBtn=tk.Button(self.frameTop,text='Buscar',padx=5,pady=5)
+        #self.txt1.grid(row=0,column=2)
         #self.txt1.insert(0,'C:\\')
 
-        self.buttonFrame=tk.Frame(self.frameMain,padx=5,pady=5)
-        self.buscarBtn=tk.Button(self.frameMain,text='Buscar',padx=5,pady=5)
-        self.buscarBtn.grid(row=0,column=3)
+        self.directlbl.pack(side='left')
+        self.addresstxt.pack(side='left')
+        self.buscarBtn.pack(side='left')
+
+        tk.Label(self.frameMain,text='Cantidad de columnas: ',padx=5,pady=5).grid(row=1,column=0, sticky='w')      
+
+        self.cFrame=tk.Frame(self.frameMain,padx=5, pady=5,borderwidth=2, relief='ridge')
+        self.cFrame.grid(row=2,column=0)
+        campoFrame(self.cFrame)
         
 class campoFrame(tk.Frame):
 
     def __init__(self,master=None):
-        super().__init__(master, bg='red')
+        super().__init__(master)
         self.master=master
         self.createWidgets()
 
@@ -42,15 +49,23 @@ class campoFrame(tk.Frame):
         nameCampolbl=tk.Label(self.master,text='Nompre del campo: ',padx=5,pady=5)
         fBuscarlbl=tk.Label(self.master,text='Forma de buscar: ',padx=5,pady=5)    
         
-        nCampolbl.grid(row=0,column=0, sticky='n')
-        nameCampolbl.grid(row=1,column=0, sticky='n')
-        fBuscarlbl.grid(row=2,column=0, sticky='n')
+        nCampolbl.grid(row=0,column=0, sticky='e')
+        nameCampolbl.grid(row=1,column=0, sticky='e')
+        fBuscarlbl.grid(row=2,column=0, sticky='e')
 
+        nCampoTbx=tk.Entry(self.master)
+        nameCampoTbx=tk.Entry(self.master)
+        fBuscarCbx=ttk.Combobox(self.master, state='readonly')
+
+        nCampoTbx.grid(row=0,column=1, sticky='n')
+        nameCampoTbx.grid(row=1,column=1, sticky='n')
+        fBuscarCbx.grid(row=2,column=1, sticky='n')
 
 
 if __name__=='__main__':
     root=tk.Tk()
     root.wm_title('EXTRACTOR PDF')
     app=v1Window(root)
+
     app.mainloop()
         
